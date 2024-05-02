adf4ec79ac8ab2c05554e90d8cc000afa67c9960
AronSn0w <52366564+Aron1244@users.noreply.github.com>
2024-05-01 22:15:53 -0400

    5° Commit
    Se agrego un boton al footer para cambiar el fondo a modo claro y obscuro a traves de JavaScript

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
-// Función para cambiar entre los modos claro y oscuro
-const cambiarModo = () => {
-  const body = document.body;
-  body.classList.toggle("light-mode");
-  body.classList.toggle("dark-mode");
 
-  // Guarda el modo actual en el almacenamiento local para mantenerlo entre sesiones
-  const modoActual = body.classList.contains("light-mode") ? "light" : "dark";
-  localStorage.setItem("modo", modoActual);
-};
-
-// Agrega un event listener al botón de cambio de modo
-document.getElementById("modo-btn").addEventListener("click", cambiarModo);
-
-// Verifica si hay un modo guardado en el almacenamiento local y aplícalo
-const modoGuardado = localStorage.getItem("modo");
-if (modoGuardado === "dark") {
-  cambiarModo();
+function toggleTheme() {
+  const content = document.getElementById('contenido');
+  if (content.classList.contains('dark-mode')) {
+    content.classList.remove('dark-mode');
+    content.classList.add('light-mode');
+  } else {
+    content.classList.remove('light-mode');
+    content.classList.add('dark-mode');
+  }
 }
-
