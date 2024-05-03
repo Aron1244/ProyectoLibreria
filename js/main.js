564aac50c0e4074bb04454723f462532e693409b
AronSn0w <52366564+Aron1244@users.noreply.github.com>
2024-05-03 15:57:34 -0400

    7° Commit
    Se modifico el boton y el codigo para cambiar de modo claro a obscuro, ahora cambia el contenedor, el navbar y el footer.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,44 @@
 
 function toggleTheme() {
   const content = document.getElementById('contenido');
+  const footer = document.getElementById('footer');
+  const navbar = document.getElementById('navbar');
   if (content.classList.contains('dark-mode')) {
     content.classList.remove('dark-mode');
     content.classList.add('light-mode');
+
+    footer.classList.remove('dark-mode-footer');
+    footer.classList.add('light-mode-footer');
+
+    navbar.classList.remove('dark-mode-footer');
+    navbar.classList.add('light-mode-footer');
+    navbar.classList.remove('navbar-dark');
   } else {
     content.classList.remove('light-mode');
     content.classList.add('dark-mode');
+
+    footer.classList.remove('light-mode-footer');
+    footer.classList.add('dark-mode-footer');
+
+    navbar.classList.remove('light-mode-footer');
+    navbar.classList.add('dark-mode-footer');
+    navbar.classList.add('navbar-dark');
   }
+}
+
+function toggleThemes() {
+  const navbar = document.getElementById('navbar');
+  
+  if (navbar.classList.contains('dark-mode-footer')) {
+    navbar.classList.remove('dark-mode-footer');
+    navbar.classList.add('light-mode-footer');
+  } else {
+    navbar.classList.remove('light-mode-footer');
+    navbar.classList.add('dark-mode-footer');
+  }
+}
+
+function toggleNavbar() {
+  const navbar = document.getElementById('navbarSupportedContent');
+  navbar.classList.toggle('show');
 }
