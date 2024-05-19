72e59e3d4928c51505bd2c46f05897f18cd0c59d
AronSn0w <52366564+Aron1244@users.noreply.github.com>
2024-05-19 13:13:48 -0400

    11° Commit
    Se hicieron modificaciones en los documentos de css, se modifico el catalogo, se agregaron estilos y se crearon las plantillas de los libros.

--- js/login.js
+++ js/login.js
@@ -1,21 +1,20 @@
-// Función para manejar el inicio de sesión
 function iniciarSesion(event) {
-    event.preventDefault(); // Evita que el formulario se envíe y la página se recargue
+    event.preventDefault();
 
     const usuario = document.getElementById('inputUsuario').value;
     const contra = document.getElementById('inputContra').value;
 
-    // Valores de "super usuario"
     const superUsuario = 'admin';
     const superContra = 'admin123';
 
-    // Verifica si el usuario y la contraseña coinciden
     if (usuario === superUsuario && contra === superContra) {
         alert('Inicio de sesión exitoso!');
-        // Redirigir a otra página o realizar alguna acción adicional aquí
+        setTimeout(function () {
+            window.location.href = 'admin.html';
+        }, 500);
     } else {
         alert('Usuario o contraseña incorrectos. Inténtelo de nuevo.');
     }
 
-    return false; // Impide el envío del formulario
+    return false;
 }
