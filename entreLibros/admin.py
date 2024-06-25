190ced4a79eec5d0b4541540333af0337561e538
Aron1244 <raycuasa80@gmail.com>
2024-06-25 17:03:20 -0400

    Se añadieron los modelos

--- entreLibros/admin.py
+++ entreLibros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Usuario,Libro,Categoria
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Libro)
+admin.site.register(Categoria)
